c0b7750e52b8c22401f2a3acb946351b090ebcac
asarup <asarup@ucsd.edu>
2023-05-25 19:56:57 -0700

    added feedback

--- grade.sh
+++ grade.sh
@@ -15,28 +15,30 @@ echo 'Finished cloning'
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
 
-file='./student-submission/ListExamples.java'
-if [[ ! -f "$file" ]]
-then
-    echo 'Missing file: ListExamples.java'
+File=./student-submission/ListExamples.java
+if [[ ! -f "$File" ]]; 
+then 
+    echo 'File does not exist. Must be ListExamples.java' 
     exit
-fi
+fi 
 
-cp ./TestListExamples.java ./student-submission/* ./grading-area
+cp ./lib/*.jar ./student-submission/* ./TestListExamples.java ./grading-area
 cp -r ./lib ./grading-area
 
 cd ./grading-area
+javac -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-1.3.jar" *.java  2>failed.txt
 
-javac -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar *.java 2>compileerr.txt
-if [[ -s compileerr.txt ]]
-then
-    echo 'The submitted ListExamples.java file did not compile. The error can be seen below:'
-    cat compileerr.txt
+if [[ -s failed.txt ]]
+then 
+    cat failed.txt
     exit
-else
-    echo 'Submission compiled successfully!'
+else 
+    echo "Compile successful. Hooray"
 fi
 
-java -cp .:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar org.junit.runner.JUnitCore TestListExamples 1>results.txt
-
-cat results.txt
+java -cp ".;..\lib\junit-4.13.2.jar;..\lib\hamcrest-1.3.jar" org.junit.runner.JUnitCore TestListExamples.java > results.txt
+tail -n 2 results.txt > score0.txt
+grep  -o --regexp='[0-9]*' score0.txt > score1.txt
+head -n -3 results.txt
+echo Number of Failures: head -n 2 score1.txt | tail -n 1 score1.txt
+echo Total number of tests: head -n 1 score1.txt
---
+++ grading-area/TestListExamples.java
@@ -0,0 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import java.util.Arrays;
+import java.util.List;
+
+class IsMoon implements StringChecker {
+  public boolean checkString(String s) {
+    return s.equalsIgnoreCase("moon");
+  }
+}
+
+public class TestListExamples {
+  @Test(timeout = 500)
+  public void testMergeRightEnd() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test
+  public void testFilter() {
+    List<String> testing = Arrays.asList("sun", "moon", "star");
+    List<String> result = ListExamples.filter(testing, new IsMoon());
+    assertEquals(Arrays.asList("moon", "MOON"), result);
+  }
+}
